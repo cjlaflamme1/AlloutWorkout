13773e9362e8c8af0d5eaab524e673a0d80548f5
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-10-10 12:58:56 -0400

    had to export router on route index. server starts

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const workoutRoutes = require('./workouts');
 // require individual route pages here
 
 const Router = express.Router();
 
+Router.use("/workouts", workoutRoutes);
 // call Router with the query paths here. 
+
+module.exports = Router;
