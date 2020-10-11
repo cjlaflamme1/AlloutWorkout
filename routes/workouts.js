5319c038c6b6900343f0653430bb821b79fbb8be
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-10-10 21:38:26 -0400

    removing some console logs

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,21 +1,6 @@
 const express = require('express');
 const db = require("../models");
-const workoutSeed = require("../seeders/seed");
 const router = express.Router();
-
-router.get('/seeds', (req, res) => {
-    console.log(workoutSeed);
-    db.Workout.deleteMany({})
-      .then(() => db.Workout.collection.insertMany(workoutSeed))
-      .then(data => {
-        console.log(data.result.n + " records inserted!");
-        process.exit(0);
-      })
-      .catch(err => {
-        console.error(err);
-        process.exit(1);
-      });
-})
 
 router.get('', (req, res) => {
     db.Workout.find({}).then((workouts) => {
@@ -25,7 +10,6 @@ router.get('', (req, res) => {
     })
 });
 router.get('/range', (req, res) => {
-    console.log('this is the range request');
     db.Workout.find({}).then((workouts) => {
         res.json(workouts);
     }).catch((err) => {
@@ -36,7 +20,6 @@ router.get('/range', (req, res) => {
 router.put('/:id', async (req, res) => {
     const id = req.params.id;
     const newExercise = req.body;
-    console.log(newExercise);
     if(id !== "undefined"){
         await db.Workout.findOneAndUpdate({'_id': id}, {'$addToSet': {'exercises': newExercise}}).catch((err) => {console.log(err)});
     } else {
@@ -49,7 +32,6 @@ router.put('/:id', async (req, res) => {
 
 
 router.post('', (req, res) => {
-    console.log(req.body);
     db.Workout.find({}).then((workouts) => {
         res.json(workouts);
     }).catch((err) => {
